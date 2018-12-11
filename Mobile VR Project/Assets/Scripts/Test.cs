21c0e110e0f9a795db6dd5c98954dfe74ea6afb0
Jo Huijae <whgmlwo1000@gmail.com>
2018-12-12 08:32:27 +0900

    added magnet function

---
+++ Mobile VR Project/Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+    private MagneticClicker mag;
+    public Transform boxTransform;
+	// Use this for initialization
+	void Start () {
+        mag = new MagneticClicker();
+        mag.init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mag.magUpdate(Input.acceleration, Input.compass.rawVector);
+        if(mag.clicked())
+        {
+            boxTransform.Rotate(new Vector3(0.0f, 45.0f, 0.0f), Space.World);
+        }
+	}
+}
